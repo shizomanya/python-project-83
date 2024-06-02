5cd6e4fda507a980190ed8923bd02be16398fb60
shizomanya <mistic_aa@inbox.ru>
2024-06-02 15:34:13 +0300

    fix

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -163,3 +163,13 @@ def id_check(id):
             conn.commit()
     flash("Page successfully checked", "alert alert-success")
     return redirect(url_for('url_added', id=id))
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
